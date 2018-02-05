7d18cc11e85769685de180a15bde6891ed10757e
Alexey Evdokimov <uncle-arthy@users.noreply.github.com>
2018-02-05 20:31:01 +0500

    Sceleton for core classes for stars, planets etc.

--- SGenCore.py
+++ SGenCore.py
@@ -1,6 +1,25 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 __author__ = 'Alexey Evdokimov'
+import random
+
+
+class Dice(object):
+    @staticmethod
+    def d20():
+        return random.randint(1, 20)
+    
+    @staticmethod
+    def d12():
+        return random.randint(1, 12)
+    
+    @staticmethod
+    def d10():
+        return random.randint(1, 10)
+    
+    @staticmethod
+    def d2():
+        return random.randint(1, 2)
 
 
 class Cluster(object):
@@ -15,14 +34,56 @@ class StarSystem(object):
     def __init__(self):
         self.stars = []  # One or more. Main star goes first
         self.planets = []  # Including asteroid and comet belts
-    
+        
+        self.make_stars()
+        
+    def make_stars(self):
+        # How many stars?
+        roll_for_stars = Dice.d20()
 
-class Star(object):
+        if roll_for_stars < 11:  # TODO: make it with NumPy and choise by weight
+            number_of_stars = 1
+        elif roll_for_stars < 17:
+            number_of_stars = 2
+        elif roll_for_stars < 19:
+            number_of_stars = 3
+        elif roll_for_stars < 20:
+            number_of_stars = 4
+        else:
+            number_of_stars = 5
+
+        for _ in range(number_of_stars):
+            self.stars.append(Star())
+        print(f"Number of stars: {number_of_stars}")
+
+class Star(object):  # TODO: make help on star classes and types
     
-    NO_TYPE = "no_type"
+    # "Common" stars spectral classes
+    NO_CLASS = "no_class_star"
+    O_CLASS = "o_class_star"
+    B_CLASS = "b_class_star"
+    A_CLASS = "a_class_star"
+    F_CLASS = "f_class_star"
+    G_CLASS = "g_class_star"
+    K_CLASS = "k_class_star"
+    M_CLASS = "m_class_star"
+    # Non-sequence stars
+    BLACK_HOLE = "black_hole"
+    NEUTRON = "neutron_star"
+    WHITE_DWARF = "white_dwarf_star"
+    BLACK_DWARF = "black_dwarf_star"
+    BROWN_DWARF = "brown_dwarf_star"
+    RED_SUPERGIANT = "red_supergiant_star"
+    
     
     def __init__(self):
-        self.star_type = Star.NO_TYPE
+        pass
+    
+    def single_star(self):
+        pass
+    
+    def binary_star(self):
+        pass
 
     
 class Planet(object):
@@ -42,3 +103,7 @@ class Planet(object):
 class Moon(object):
     def __init__(self):
         self.parent_planet = None
+
+
+if __name__ == '__main__':
+    ss = StarSystem()
