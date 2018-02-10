7a14d886f642ec1a31b62e04313ee1ca94b7816a
Alexey Evdokimov <uncle-arthy@users.noreply.github.com>
2018-02-11 02:40:10 +0500

    Star core object done. Better :)

--- SGenCore.py
+++ SGenCore.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 __author__ = 'Alexey Evdokimov'
 import random
+# TODO: import NumPy and implement all table choises with "weight choises"
 
 
 class Dice(object):
@@ -19,6 +20,31 @@ class Dice(object):
         result += modifier
         
         return result
+    
+    @staticmethod
+    def complex_roll(sides, modifier, num_of_dices=1, positive=False):
+        '''
+        Makes roll of XdY+Z type
+        :param positive: If true, result is not less than 0
+        :param sides: Y in XdY+Z
+        :param modifier: Z in XdY+Z
+        :param num_of_dices: X in XdY+Z
+        :return: result
+        '''
+        
+        result = 0
+        
+        for _ in range(num_of_dices):
+            result += random.randint(1, sides)
+            
+        result += modifier
+        
+        if positive:
+            if result < 0:
+                result = 0
+        
+        return result
+
 
 class Cluster(object):
     def __init__(self):
@@ -30,18 +56,22 @@ class Cluster(object):
 
 class StarSystem(object):
     def __init__(self, system_name="Unknown system"):
-        self.star_core = []  # One or more.
+        self.star_core = None  # One or more.
         self.planets = []  # Including asteroid and comet belts
         self.system_name = system_name
         
-        self.make_stars()
-        self.make_planets()
+        self.make_stars()  # Make star(s)
+        
+        if not self.planets:  # If there are planets at all
+            self.make_planets()
         
     def make_stars(self):
         # How many stars?
         roll_for_stars = Dice.roll(20)
 
-        if roll_for_stars < 11:  # TODO: make it with NumPy and choise by weight
+        # According to Table G58 in GMG
+        # TODO: make it with NumPy and choise by weight
+        if roll_for_stars < 11:
             number_of_stars = 1
         elif roll_for_stars < 17:
             number_of_stars = 2
@@ -58,6 +88,10 @@ class StarSystem(object):
         pass
             
     def print_chart(self):
+        """
+        Just a service placeholder function
+        :return:
+        """
         print(f"\n---===  {self.system_name}  ===---\n")
         print("Stars:")
         for place in self.star_core.stars:
@@ -103,6 +137,8 @@ class StarCore(object):  # TODO: make help on star classes and types
             self.number_of_planets = 0
     
     def single_star(self):
+        
+        # According to Table G59 in GMG
         roll = Dice.roll(20)
         if roll == 1:
             self.stars.append(StarCore.O_B_CLASS)
@@ -144,6 +180,8 @@ class StarCore(object):  # TODO: make help on star classes and types
             self.non_sequence_star()
             
     def binary_star(self):
+        
+        # According to Table G60 in GMG
         roll = Dice.roll(20)
         if roll == 1:
             self.stars.append([StarCore.O_B_CLASS, StarCore.WHITE_DWARF])
@@ -207,6 +245,8 @@ class StarCore(object):  # TODO: make help on star classes and types
                 self.number_of_planets = Dice.planets_roll(12, -7)
                 
     def non_sequence_star(self):
+        
+        # According to second part of Table G59 in GMG
         roll = Dice.roll(20)
         if roll == 1:
             self.stars.append(StarCore.BLACK_HOLE)
@@ -247,17 +287,40 @@ class StarCore(object):  # TODO: make help on star classes and types
                 
     
 class Planet(object):
+    # Planet type
+    ASTEROID_BELT = "Asteroid belt"
+    COMET_BELT = "Comet belt"
+    SUB_TERRAN = "Sub-terran"
+    TERRAN = "Terran"
+    SUPER_TERRAN = "Super terran"
+    GAS_GIANT_LARGE = "Gas giant, large"
+    GAS_GIANT_SMALL = "Gas giant, small"
     
-    ASTEROID_BELT = "asteroid_belt"
-    COMET_BELT = "comet_belt"
-    TERRAN_TYPE = "terran_type"
-    GAS_GIANT = "gas_giant"
+    # Planet temperature
+    TEMPERATE = "Temperate"
+    HOT = "Hot"
+    COLD = "Cold"
     
-    def __init__(self):
-        self.parent_system = None
-        self.planet_type = Planet.TERRAN_TYPE
-        self.graph_type = [0, 0, 0, 0, 0]  # GRAPH type of planet according to rulebook
+    # Orbit track possible distances as in Table G61 in GMG
+    OT_1 = [0.7, 1.0, 1.5, 2.0, 3.0, 5.0, 10.0, 15.0, 20.0, 25.0, 30.0, 35.0, 40.0, 45.0, 50.0, 55.0]
+    OT_2 = [0.3, 0.4, 0.7, 1.0, 1.5, 2.0, 3.0, 5.0, 10.0, 15.0, 20.0, 25.0, 30.0, 35.0, 40.0, 45.0]
+    OT_3 = [0, 0, 0, 0, 0, 0.1, 0.2, 0.3, 0.4, 0.7, 1.0, 1.5, 2.0, 3.0, 5.0, 10.0]
+    OT_4 = [1.5, 2.0, 3.0, 5.0, 10.0, 15.0, 20.0, 25.0, 30.0, 35.0, 40.0, 45.0, 50.0, 55.0, 0, 0]
+    OT_5 = [0, 0, 0, 0, 0, 0, 1.5, 2.0, 3.0, 5.0, 10.0, 15.0, 20.0, 30.0, 40.0, 50.0]
+    ORBIT_TRACKS = [OT_1, OT_2, OT_3, OT_4, OT_5]
+    
+    PLANET_TYPES_ON_TRACK = [(6, -3), (4, -1), (4, 0), (4, 3),
+                             (4, 4), (6, 4), (6, 6, 2), (6, 6, 2),
+                             (4, 10, 2), (4, 10, 2), (8, 10), (8, 10),
+                             (6, 12), (6, 12), (8, 12), (8, 12)]
+    
+    def __init__(self, orbit_ring, parent_system=None):
+        self.parent_system = parent_system
+        self.planet_type = Planet.TERRAN
+        self.temperature = Planet.TEMPERATE
+        self.graph_type = [0, 0, 0, 0, 0]  # GRAPH type of planet according GMG
         self.moons = []
+        self.orbit_ring = orbit_ring
 
 
 class Moon(object):
